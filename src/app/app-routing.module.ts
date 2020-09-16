d205a89edaeb5041055ecb476283fa6c9216fb2d
Omar Marenco <omar@omarenco.com>
2020-09-15 20:08:17 -0500

    Organizamos las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './auth/login/login.component';
+import {RegisterComponent} from './auth/register/register.component';
+import {ForgetComponent} from './auth/forget/forget.component';
 
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: '/auth/register',
-    pathMatch: 'full'
-  },
-  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  }];
+  },
+  {
+    path: 'otraruta',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
